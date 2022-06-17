425f2116053350ec9944c708ba1bbd8c886d60eb
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-06-17 16:12:55 +0900

    jblog #3

--- jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
@@ -1,5 +1,34 @@
 package com.douzone.jblog.controller;
 
+import java.util.Optional;
+
+@Controller
+
+@RequestMapping("/{id:(?!assets).*}")
 public class BlogController {
 
+	@RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}"})
+	public String index(
+			@PathVariable("id") String id,
+			@PathVariable("pathNo1") Optional<Long> pathNo1,
+			@PathVariable("pathNo1") Optional<Long> pathNo2) {
+		
+		Long categoryNo = 0L;
+		Long postNo = 0L;
+		
+		if(pathNo2.isPresent()) {
+			categoryNo = pathNo1.get();
+			postNo = pathNo2.get();
+		} else if(pathNo1.isPresent()) {
+			categoryNo = pathNo1.get();
+		}
+		
+		categoryService.getCategories(id, categoryNo)
+		
+	}
+	
+	@RequestMapping("/admin/basic")
+	public String adminBasic(@PathVariable("id") String id) {
+		
+	}
 }
